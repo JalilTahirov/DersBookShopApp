cd8ab20060287b389611901d9273a219dffcb4a3
Jalil T <jalil.tahirov@outlook.com>
2019-01-19 16:40:12 +0400

    initial commit

--- Ders2App/Startup.cs
+++ Ders2App/Startup.cs
@@ -14,9 +14,10 @@ namespace Ders2App
 {
     public class Startup
     {
+   
 
 
-
+        //
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
